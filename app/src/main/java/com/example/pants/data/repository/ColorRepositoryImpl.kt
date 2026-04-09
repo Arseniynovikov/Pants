9b8403cb54c58eaecb87c60f17d11f02be0f8b9c
Arseniynovikov <arseniydfg@gmail.com>
2026-04-09 20:26:17 +0300

    PANTS-4 optimize of repository implementation

--- app/src/main/java/com/example/pants/data/repository/ColorRepositoryImpl.kt
+++ app/src/main/java/com/example/pants/data/repository/ColorRepositoryImpl.kt
@@ -1,28 +1,52 @@
 package com.example.pants.data.repository
 
+import android.util.Log
 import com.example.pants.data.network.ColorApiService
 import com.example.pants.domain.model.ColorModel
 import com.example.pants.domain.repository.ColorRepository
 import com.example.pants.domain.utils.generateRandomColor
 import com.example.pants.data.mappers.toColorModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class ColorRepositoryImpl(
     private val apiService: ColorApiService,
 ) : ColorRepository {
 
-    override suspend fun getRandomColors(count: Int): Result<Set<ColorModel>> = runCatching {
-        val colorList = mutableListOf<ColorModel>()
+    override suspend fun getRandomColors(count: Int): Result<Set<ColorModel>> = withContext(
+        Dispatchers.IO
+    ) {
+        runCatching {
+            val colorList = mutableListOf<ColorModel>()
 
-        while (colorList.size < count) {
-            val color = apiService.getColor(generateRandomColor()).toColorModel()
-            val doesntContainCommon = color.name.lowercase(Locale.getDefault()) !in COMMON_USE_NAMES
-            val isDistinct = color !in colorList
-            if (doesntContainCommon && isDistinct) {
-                colorList.add(color)
+            while (colorList.size < count) {
+
+                val neededCount = count - colorList.size
+
+                val jobs = (1..neededCount * 2).map {
+                    async {
+                        val response = apiService.getColor(generateRandomColor())
+                        response.toColorModel()
+                    }
+                }
+
+                jobs.awaitAll()
+                    .filter { it.isValid() }
+                    .forEach { if (colorList.size < count) colorList.add(it) }
+
             }
+            colorList.toSet()
+
         }
-        colorList.toSet()
+    }
+
+    private fun ColorModel.isValid(): Boolean {
+        val nameLower = name.lowercase(Locale.getDefault())
+
+        return !COMMON_USE_NAMES.any { nameLower.contains(it) }
     }
 
     private companion object {
@@ -60,3 +84,5 @@ class ColorRepositoryImpl(
         )
     }
 }
+
+
